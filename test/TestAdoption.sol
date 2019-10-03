1f4937cad6d8c2afa8c400f1c3149358233338cd
shevek <shevek@anarres>
2019-10-03 23:08:20 +0100

    wrote test contract

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -2,38 +2,38 @@ pragma solidity ^0.5.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/Adoption.sol";
+import "../contracts/Stateless.sol";
 
-contract TestAdoption {
- // The address of the adoption contract to be tested
- Adoption adoption = Adoption(DeployedAddresses.Adoption());
+contract TestStateless {
+ // The address of the Stateless contract to be tested
+ Stateless stateless = Stateless(DeployedAddresses.Stateless());
 
- // The id of the pet that will be used for testing
+ // The id of the profile that will be used for testing
  uint expectedhumanId = 8;
 
- //The expected owner of adopted pet is this contract
- address expectedAdopter = address(this);
+ //The expected verifier of verified profile is this contract
+ address expectedVerifier = address(this);
 
- // Testing the adopt() function
- function testUserCanAdoptPet() public {
-   uint returnedId = adoption.adopt(expectedhumanId);
+ // Testing the verify() function
+ function testUserCanVerifyProfile() public {
+   uint returnedId = verification.verify(expectedhumanId);
 
-   Assert.equal(returnedId, expectedhumanId, "Adoption of the expected pet should match what is returned.");
+   Assert.equal(returnedId, expectedhumanId, "The verifier of the expected profile should match what is returned.");
  }
 
-// Testing retrieval of a single pet's owner
-function testGetAdopterAddressByhumanId() public {
-  address adopter = adoption.adopters(expectedhumanId);
+// Testing retrieval of a single profile's verifier
+function testGetVerifierAddressByhumanId() public {
+  address verifier = verify.verifiers(expectedhumanId);
 
-  Assert.equal(adopter, expectedAdopter, "Owner of the expected pet should be this contract");
+  Assert.equal(verifier, expectedVerifier, "Verifier of the expected profile should be this contract");
 }
 
-// Testing retrieval of all pet owners
-function testGetAdopterAddressByhumanIdInArray() public {
-  // Store adopters in memory rather than contract's storage
-  address[16] memory adopters = adoption.getVerifiers();
+// Testing retrieval of all profile verifiers
+function testGetVerifierAddressByhumanIdInArray() public {
+  // Store verifiers in memory rather than contract's storage
+  address[16] memory verifiers = verify.getVerifiers();
 
-  Assert.equal(adopters[expectedhumanId], expectedAdopter, "Owner of the expected pet should be this contract");
+  Assert.equal(verifiers[expectedhumanId], expectedVerifer, "Verifier of the expected profile should be this contract");
 }
 
 }
